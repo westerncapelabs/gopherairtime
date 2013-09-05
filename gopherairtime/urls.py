d399bf16f718e736008cdf353703b0ba11c33d82
Mark Gituma <gitumarkk@gmail.com>
2013-09-05 17:51:28 +0200

    registered end-point with urls

--- gopherairtime/urls.py
+++ gopherairtime/urls.py
@@ -7,7 +7,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'skeleton.views.home', name='home'),
-    # url(r'^skeleton/', include('skeleton.foo.urls')),
+    url(r'^', include('hotsocket.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
