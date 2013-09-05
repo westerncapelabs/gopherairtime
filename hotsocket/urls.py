75b5fcc16d8f5fff4686ae47c5b30502f201c3b6
Mark Gituma <gitumarkk@gmail.com>
2013-09-05 17:51:39 +0200

    registered end point with urls

---
+++ hotsocket/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import patterns, url, include
+from hotsocket.api import RechargeResource
+from tastypie.api import Api
+
+
+# Setting the API base name and registering the API resources using
+# Tastypies API function
+api_resources = Api(api_name='v1')
+api_resources.register(RechargeResource())
+api_resources.prepend_urls()
+
+# Setting the urlpatterns to hook into the api urls
+urlpatterns = patterns('',
+    url(r'^api/', include(api_resources.urls))
+)
