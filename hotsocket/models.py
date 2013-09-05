0fa4bd77d572ffb8fe72b6e85394fba20713abbe
Mark Gituma <gitumarkk@gmail.com>
2013-09-05 17:50:53 +0200

    created reacharge models

--- hotsocket/models.py
+++ hotsocket/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+
+class Recharge(models.Model):
+    """
+    - For the client to analyse users recharge
+    - For admin to analyse trend
+    """
+    msisdn = models.BigIntegerField()
+    product_code = models.CharField(max_length=20)
+    denomination = models.IntegerField()
+    reference = models.BigIntegerField(unique=True, null=True)
+    notes = models.CharField(max_length=100, verbose_name=u'Notes', null=True)
+    status = models.CharField(max_length=20)
+    hot_socket_ref = models.BigIntegerField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    status_confirmed_at = models.DateTimeField(null=True)
+
+    def __unicode__(self):
+        return "%s" % self.msisdn_recharge
+
+    class Meta:
+        verbose_name = "Recharge"
+
+
+class RechargeError(models.Model):
+    """
+    For back end analysis
+    For admin to analyse trends
+    """
+    recharge_error = models.ForeignKey(Recharge,
+                                       verbose_name=u'Msisdn Errored',
+                                       related_name='recharge_error')
+    error_id = models.IntegerField()
+    error_message = models.CharField(max_length=255)
+    tries = models.IntegerField()  # Number of times recharge attempted
+    last_attempt_at = models.DateTimeField(null=True)
