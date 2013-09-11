5decdfff1e5cda1494e69bc72cd239001b9aad63
gitumarkk <gitumarkk@gitumarkk-Satellite-C850D.(none)>
2013-09-11 18:12:21 +0200

    Adding the login credentials to the API settings

--- gopherairtime/api_settings.py
+++ gopherairtime/api_settings.py
@@ -1,6 +1,7 @@
 # This stores all the settings that will be used in the api
 
 HOTSOCKET_BASE = "http://api.hotsocket.co.za:8080/"
+
 HOTSOCKET_RESOURCES = {
 	"login": "test/login",
 	"recharge": "test/recharge",
@@ -8,4 +9,8 @@ HOTSOCKET_RESOURCES = {
     "statement": "test/statement",
     "balance": "test/balance",
 }
+
 HOTSOCKET_USERNAME = "trial_acc_1212"
+HOTSOCKET_PASSWORD = "tr14l_l1k3m00n"
+
+TOKEN_DURATION = 110  # Minutes
