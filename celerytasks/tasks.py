9befd11098f5db4005ef32412575ba8b187eb399
gitumarkk <gitumarkk@gitumarkk-Satellite-C850D.(none)>
2013-09-11 11:41:52 +0200

    adding basic celery tasks

--- celerytasks/tasks.py
+++ celerytasks/tasks.py
@@ -1,5 +1,7 @@
 from celery.decorators import task
+from celery.utils.log import get_task_logger
 from hotsocket.models import Recharge
+from django.conf import settings
 import requests
 import json
 
@@ -17,15 +19,26 @@ def hotsocket_login():
 	json_response = response.json()
 	print json_response["response"]["token"]
 
+@task()
+def run_queries():
+	"""
+	Main purpose of this is to call functions that query database and to chain them
+	"""
+	recharge_query().delay()
+	status_query().delay()
+	errors_query().delay()
 
 
 @task()
-def recharge_msisdn():
+def recharge_query():
+	"""
+	Queries database and passes it to the get_recharge() task asynchronously
+	"""
 	queryset = Recharge.objects.filter(hot_socket_ref=None).all()
 
 	for query in queryset:
 		reference = int(str(query.msisdn) + str(query.id)) * 3
-		data = {"username": "trial_acc_1212",
+		data = {"username": settings.HOTSOCKET_USERNAME,
 				"token": "awo7LCrtyF5qG5GDyvfpCpy2D3ALcdnoSdZ945UJsLeV9dxa2DwjlvMOErx7tckEzLAP+dlHyQHvNdDaYHc/raenmL30FdtbSMsqzurtBYI=",
 				"recipient_msisdn": query.msisdn,
 				"product_code": query.product_code,
@@ -33,8 +46,22 @@ def recharge_msisdn():
 				"network_code": "VOD",
 				"reference": reference,
 				"as_json": True}
+		get_recharge.delay(data)
 
-		url = "http://api.hotsocket.co.za:8080/test/recharge/"
+
+@task()
+def status_query():
+	pass
+
+
+@task()
+def errors_query():
+	pass
+
+
+@task()
+def get_recharge(data):
+		url = "%s%s" % (settings.HOTSOCKET_BASE, settings.HOTSOCKET_RESOURCES["recharge"])
 		headers = {'content-type': 'application/json'}
 		response = requests.post(url, data=data)
 		json_response = response.json()
