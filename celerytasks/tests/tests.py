0dbfd3cfb9bbe0dfe2dbad1650b563186ada148d
gitumarkk <gitumarkk@gitumarkk-Satellite-C850D.(none)>
2013-09-11 18:14:06 +0200

    Adding tests for login API - Only works if connected to server

--- celerytasks/tests/tests.py
+++ celerytasks/tests/tests.py
@@ -3,9 +3,11 @@ from django.test.utils import override_settings
 from django.core.urlresolvers import reverse
 import json
 from recharge.models import Recharge
-from celerytasks.tasks import run_queries
+from celerytasks.models import StoreToken
+from celerytasks.tasks import run_queries, hotsocket_login
 
-class TestHotSocket(TestCase):
+
+class TestRecharge(TestCase):
     fixtures = ["test_recharge.json"]
 
     @override_settings(CELERY_EAGER_PROPAGATES_EXCEPTIONS = True,
@@ -23,3 +25,17 @@ class TestHotSocket(TestCase):
         [self.assertIsNotNone(obj.reference) for obj in query]
         [self.assertIsNotNone(obj.recharge_system_ref) for obj in query]
 
+
+class TestLogin(TestCase):
+    @override_settings(CELERY_EAGER_PROPAGATES_EXCEPTIONS = True,
+                       CELERY_ALWAYS_EAGER = True,
+                       BROKER_BACKEND = 'memory',)
+
+
+    def test_query_function(self):
+        # import pdb; pdb.set_trace()
+        hotsocket_login.delay()
+        query = StoreToken.objects.all()
+        [self.assertIsNotNone(obj.token) for obj in query]
+        [self.assertIsNotNone(obj.updated_at) for obj in query]
+        [self.assertIsNotNone(obj.expire_at) for obj in query]
