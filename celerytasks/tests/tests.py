0237dbcd4e49a9fe72f6adb753bd9ccc1a46aedc
gitumarkk <gitumarkk@gitumarkk-Satellite-C850D.(none)>
2013-09-11 15:33:23 +0200

    Adding the required unittests

--- celerytasks/tests/tests.py
+++ celerytasks/tests/tests.py
@@ -1,16 +1,25 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from django.test.utils import override_settings
+from django.core.urlresolvers import reverse
+import json
+from recharge.models import Recharge
+from celerytasks.tasks import run_queries
 
+class TestHotSocket(TestCase):
+    fixtures = ["test_recharge.json"]
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    @override_settings(CELERY_EAGER_PROPAGATES_EXCEPTIONS = True,
+                       CELERY_ALWAYS_EAGER = True,
+                       BROKER_BACKEND = 'memory',)
+
+    def test_data_loaded(self):
+        query = Recharge.objects.all()
+        self.assertEqual(len(query), 2)
+
+    def test_query_function(self):
+        # import pdb; pdb.set_trace()
+        run_queries.delay()
+        query = Recharge.objects.all()
+        [self.assertIsNotNone(obj.reference) for obj in query]
+        [self.assertIsNotNone(obj.recharge_system_ref) for obj in query]
+
