a08e8b1c0ca759351a9ccc11b1c05c7f415b19c2
gitumarkk <gitumarkk@gmail.com>
2013-09-25 16:17:58 +0200

    added Projects Model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Project(models.Model):
+    """
+    Each Project will have a user
+    """
+    users_projects = models.ForeignKey(User)
+    name = models.CharField(max_length=30)
+    budget = models.IntegerField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return "%s" % self.name
+
+    class Meta:
+        verbose_name = "Project"
