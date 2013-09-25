773d9bfb7ecffffdc2f25e9cdf62586267e4175e
gitumarkk <gitumarkk@gmail.com>
2013-09-25 16:18:07 +0200

    registered projects model

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,15 @@
 from django.contrib.auth.models import User
 from django.db import models
 from tastypie.models import create_api_key
+from users.models import Project
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 
 models.signals.post_save.connect(create_api_key, sender=User)
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Project, ProjectAdmin)
