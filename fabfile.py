c6f3a06a8bcc8561f69a22f55e2a1a62270b8962
Mike Jones <mike@westerncapelabs.com>
2013-10-08 23:36:29 +0200

    Added production config and fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,19 +1,25 @@
-from fabric.api import cd, sudo, env
+from fabric.api import cd, sudo, env, abort
+from fabric.contrib.console import confirm
 import os
 
-expected_vars = [
-    'PROJECT',
-]
+PROJECT = os.environ.get('PROJECT', 'gopherairtime')
+DEPLOY_USER = os.environ.get('DEPLOY_USER', 'mike')
 
-for var in expected_vars:
-    if var not in os.environ:
-        raise Exception('Please specify %s environment variable' % (
-            var,))
 
-PROJECT = os.environ['PROJECT']
-USER = os.environ.get('USER', 'jmbo')
+def _create_paths():
+    env.path = os.path.join('/', 'srv', 'wcl', env.confprefix, PROJECT)
 
-env.path = os.path.join('var', 'praekelt', PROJECT)
+
+def staging():
+    env.confprefix = 'staging'
+
+
+def prod():
+    env.confprefix = 'prod'
+    if confirm("You are about to deploy to PRODUCTION. ARE YOU REALLY SURE?"):
+        _create_paths()
+    else:
+        abort("Aborting because you got scared... whimp.")
 
 
 def restart():
@@ -23,14 +29,14 @@ def restart():
 
 def deploy():
     with cd(env.path):
-        sudo('git pull', user=USER)
+        sudo('git pull', user=DEPLOY_USER)
         sudo('ve/bin/python manage.py syncdb --migrate --noinput',
-             user=USER)
+             user=DEPLOY_USER)
         sudo('ve/bin/python manage.py collectstatic --noinput',
-             user=USER)
+             user=DEPLOY_USER)
 
 
 def install_packages(force=False):
     with cd(env.path):
         sudo('ve/bin/pip install %s -r requirements.pip' % (
-             '--upgrade' if force else '',), user=USER)
+             '--upgrade' if force else '',), user=DEPLOY_USER)
