35baa0e1b421ec0627dd6fa01411dbcdf4e2717f
gitumarkk <gitumarkk@gmail.com>
2013-10-09 16:50:00 +0200

    created project api endpoint

--- recharge/urls.py
+++ recharge/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, url, include
-from recharge.api import RechargeResource
+from recharge.api import RechargeResource, ProjectResource
 from tastypie.api import Api
 
 
@@ -7,6 +7,7 @@ from tastypie.api import Api
 # Tastypies API function
 api_resources = Api(api_name='v1')
 api_resources.register(RechargeResource())
+api_resources.register(ProjectResource())
 api_resources.prepend_urls()
 
 # Setting the urlpatterns to hook into the api urls
