4e748c8c350cfaf6ece170f9a2e4095f424ad151
gitumarkk <gitumarkk@gmail.com>
2013-10-09 16:49:43 +0200

    changed the verbose name to be more user friendly

--- users/models.py
+++ users/models.py
@@ -6,7 +6,7 @@ class Project(models.Model):
     """
     Each Project will have a user
     """
-    users_projects = models.ForeignKey(User)
+    users_projects = models.ForeignKey(User, verbose_name=u'Project Belongs To')
     name = models.CharField(max_length=30)
     budget = models.IntegerField(null=True, blank=True)
     recharge_limit = models.IntegerField()
