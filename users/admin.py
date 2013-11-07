d165a48033a77da2216c421ffe489d7f89155b77
gitumarkk <gitumarkk@gmail.com>
2013-11-07 17:27:56 +0200

    added GopherAccount to admin

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 from tastypie.models import create_api_key
-from users.models import Project
+from users.models import Project, GopherAirtimeAccount
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
@@ -12,4 +12,10 @@ models.signals.post_save.connect(create_api_key, sender=User)
 class ProjectAdmin(admin.ModelAdmin):
     pass
 
+
+class GopherAirtimeAccountAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(Project, ProjectAdmin)
+admin.site.register(GopherAirtimeAccount, GopherAirtimeAccountAdmin)
