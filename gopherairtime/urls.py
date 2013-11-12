2ddd028ad5060df343aacb450552de6cd1b91633
gitumarkk <gitumarkk@gmail.com>
2013-11-12 11:10:34 +0200

    added the url path

--- gopherairtime/urls.py
+++ gopherairtime/urls.py
@@ -14,4 +14,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
+    # Django Registration
+    (r'^accounts/', include('registration.backends.simple.urls')),
 )
