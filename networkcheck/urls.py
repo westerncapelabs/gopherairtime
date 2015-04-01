f1390f3876e65508f23d2d47429f4cc8de8a1dfc
Mike Jones <mike@westerncapelabs.com>
2015-04-01 10:59:24 +0200

    Clear out v1 for v2 skel

--- networkcheck/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, url
-from networkcheck import views
-
-
-urlpatterns = patterns('',
-    url(r'^networkcheck/$', views.index, name='index'),
-)
--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-/srv/wcl/prod/gopherairtime/manage.py syncdb --noinput --migrate
-/srv/wcl/prod/gopherairtime/manage.py collectstatic --noinput
-
-supervisord -n
