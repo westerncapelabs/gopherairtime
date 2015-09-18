b2fbe3a15c71340224c9ef571678d4c30bab9845
Lindani <lindani@projectcodex.co>
2015-09-18 12:54:32 +0200

    updated file

--- recharges/models.py
+++ recharges/models.py
@@ -9,7 +9,7 @@ class Recharge(models.Model):
     - Nice docstring please
     """
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    amount = models.DecimalField(max_digits=5, decimal_places=2)
+    amount = models.DecimalField(max_digits=9, decimal_places=2)
     msisdn = models.CharField(max_length=20)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
