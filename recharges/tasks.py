5db2ee20fbe8ecdf1d432fd23c21702233f1bba7
Mike Jones <mike@westerncapelabs.com>
2015-09-24 15:02:35 +0200

    Refactor the task for getting token to store it in DB

--- recharges/tasks.py
+++ recharges/tasks.py
@@ -1,7 +1,10 @@
-from celery.task import Task
-from celery.utils.log import get_task_logger
 import requests
 
+from django.conf import settings
+from celery.task import Task
+from celery.utils.log import get_task_logger
+
+from .models import Account
 
 logger = get_task_logger(__name__)
 
@@ -21,8 +24,13 @@ class Hotsocket_Login(Task):
         l.info("Logging into hotsocket")
         auth = {'username': 'trial_acc_1212', 'password': 'tr14l_l1k3m00n',
                 'as_json': True}
-        r = requests.post("http://api.hotsocket.co.za:8080/test/login",data=auth)
+        r = requests.post("%s/login" % settings.HOTSOCKET_API_ENDPOINT,
+                          data=auth)
         result = r.json()
-        return result["response"]["token"]
+        token = result["response"]["token"]
+        account = Account()
+        account.token = token
+        account.save()
+        return True
 
 hotsocket_login = Hotsocket_Login()
